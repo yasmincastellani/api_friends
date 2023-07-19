34f4788f0a10816d8aaedc73926f1924f2953ba1
Yasmin <yasmincastellani@gmail.com>
2023-07-18 22:05:24 -0300

    melhorando as rotas ✨✨

--- src/routes.py
+++ src/routes.py
@@ -8,63 +8,20 @@ routes = APIRouter()
 connector = SQLiteConnector()
 
 
-@routes.get("/all", response_model=List[Character], response_description="Esse endpoint retorna todos os personagens", tags=["All"])
-def all():
+@routes.get("/all", response_model=List[Character], response_description="Esse endpoint retorna todos os personagens",
+            tags=["All"])
+def get_all():
     connector.connect()
     characters = connector.read_all()
     connector.close()
     return characters
 
 
-@routes.get("/howyoudoin", response_model=Character, response_description="Esse endpoint retorna características do Joey", tags=["Joey"])
-def joey():
+@routes.get("/character", response_model=Character,
+            response_description="Esse endpoint retorna características do Joey", tags=["Joey"])
+def joey(character: str):
     connector.connect()
-    character = connector.get_by_name("Joey Tribbiani")
-    print(character)
-    connector.close()
-    return character
-
-
-@routes.get("/unagi", response_model=Character, response_description="Esse endpoint retorna características do Ross", tags=["Ross"])
-def ross():
-    connector.connect()
-    character = connector.get_by_name("Ross Geller")
-    print(character)
-    connector.close()
-    return character
-
-
-@routes.get("/rachelgreen", response_model=Character, response_description="Esse endpoint retorna características da Rachel", tags=["Rachel"])
-def rachel():
-    connector.connect()
-    character = connector.get_by_name("Rachel Green")
-    print(character)
-    connector.close()
-    return character
-
-
-@routes.get("/couldibemorefunny", response_model=Character, response_description="Esse endpoint retorna características do Chandler", tags=["Chandler"])
-def chandler():
-    connector.connect()
-    character = connector.get_by_name("Chandler Bing")
-    print(character)
-    connector.close()
-    return character
-    
-
-@routes.get("/cleaningfreak", response_model=Character, response_description="Esse endpoint retorna características da Monica", tags=["Monica"])
-def monica():
-    connector.connect()
-    character = connector.get_by_name("Monica Geller")
-    print(character)
-    connector.close()
-    return character
-
-
-@routes.get("/smellycat", response_model=Character, response_description="Esse endpoint retorna características da Phoebe", tags=["Phoebe"])
-def phoebe():
-    connector.connect()
-    character = connector.get_by_name("Phoebe Buffay")
+    character = connector.get_by_name(character)
     print(character)
     connector.close()
     return character
